7cc6c0fadc5724f1f6c236976310b526a4d337de
Cefalu Wave <luke.evan.graham@gmail.com>
2019-07-20 23:13:32 -0700

    removing footer

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,6 @@ function App() {
           <Route exact path="/saved" component={Saved} />
           <Route component={Search} />
         </Switch>
-        <Footer />
       </div>
     </Router>
   );
