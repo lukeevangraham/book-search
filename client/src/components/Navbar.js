3a8462b14f1d1f5576a1ef26c760733d42567abd
Cefalu Wave <luke.evan.graham@gmail.com>
2019-08-03 22:31:47 -0700

    mobile navbar working

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,6 +4,17 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <Link
@@ -14,10 +25,10 @@ function Navbar() {
           >
             Google Books
           </Link>
-          <li className="nav-item active">
+          {/* <li className="nav-item active"> */}
             <Link
               to="/search"
-              className={
+              className={"nav-item" + 
                 window.location.pathname === "/search"
                   ? "nav-link active"
                   : "nav-link"
@@ -25,11 +36,11 @@ function Navbar() {
             >
               Search
             </Link>
-          </li>
+          {/* </li> */}
           <li className="nav-item">
             <Link
               to="/saved"
-              className={
+              className={"nav-item" +
                 window.location.pathname === "/saved"
                   ? "nav-link active"
                   : "nav-link"
